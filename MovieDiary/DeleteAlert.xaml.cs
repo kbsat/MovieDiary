0fcd45a70ae5f8334e47e03a37c8550dde2ca3fc
unknown <kbsat@naver.com>
2020-05-22 01:17:25 +0900

    ReviewWindow 제작, DeleteAlert(경고창) 제작 및 조절, 별점 체크 기능 구현

---
+++ MovieDiary/DeleteAlert.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MovieDiary
+{
+    /// <summary>
+    /// DeleteAlert.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class DeleteAlert : Window
+    {
+        public bool isDelete = false;
+        public DeleteAlert()
+        {
+            InitializeComponent();
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            isDelete = false;
+            this.Close();
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            isDelete = true;
+            this.Close();
+        }
+    }
+}
--- MovieDiary/ReviewWindow.xaml.cs
+++ MovieDiary/ReviewWindow.xaml.cs
@@ -23,5 +23,120 @@ namespace MovieDiary
         {
             InitializeComponent();
         }
+
+        private void star_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            BitmapImage star = new BitmapImage(new Uri("star.png", UriKind.Relative));
+            BitmapImage unstar = new BitmapImage(new Uri("unstar.png", UriKind.Relative));
+            if (sender.Equals(star_1))
+            {
+                star_1.Source = star;
+                star_2.Source = unstar;
+                star_3.Source = unstar;
+                star_4.Source = unstar;
+                star_5.Source = unstar;
+            }
+            else if (sender.Equals(star_2))
+            {
+                star_1.Source = star;
+                star_2.Source = star;
+                star_3.Source = unstar;
+                star_4.Source = unstar;
+                star_5.Source = unstar;
+
+            }
+            else if (sender.Equals(star_3))
+            {
+                star_1.Source = star;
+                star_2.Source = star;
+                star_3.Source = star;
+                star_4.Source = unstar;
+                star_5.Source = unstar;
+            }
+            else if(sender.Equals(star_4))
+            {
+                star_1.Source = star;
+                star_2.Source = star;
+                star_3.Source = star;
+                star_4.Source = star;
+                star_5.Source = unstar;
+
+            }
+            else if(sender.Equals(star_5))
+            {
+                star_1.Source = star;
+                star_2.Source = star;
+                star_3.Source = star;
+                star_4.Source = star;
+                star_5.Source = star;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void star_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            BitmapImage star = new BitmapImage(new Uri("star.png", UriKind.Relative));
+            BitmapImage unstar = new BitmapImage(new Uri("unstar.png", UriKind.Relative));
+            if (sender.Equals(star_1))
+            {
+                star_1.Source = star;
+                star_2.Source = unstar;
+                star_3.Source = unstar;
+                star_4.Source = unstar;
+                star_5.Source = unstar;
+            }
+            else if (sender.Equals(star_2))
+            {
+                star_1.Source = star;
+                star_2.Source = star;
+                star_3.Source = unstar;
+                star_4.Source = unstar;
+                star_5.Source = unstar;
+
+            }
+            else if (sender.Equals(star_3))
+            {
+                star_1.Source = star;
+                star_2.Source = star;
+                star_3.Source = star;
+                star_4.Source = unstar;
+                star_5.Source = unstar;
+            }
+            else if (sender.Equals(star_4))
+            {
+                star_1.Source = star;
+                star_2.Source = star;
+                star_3.Source = star;
+                star_4.Source = star;
+                star_5.Source = unstar;
+
+            }
+            else if (sender.Equals(star_5))
+            {
+                star_1.Source = star;
+                star_2.Source = star;
+                star_3.Source = star;
+                star_4.Source = star;
+                star_5.Source = star;
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteAlert dw = new DeleteAlert();
+            dw.ShowDialog();
+            if(dw.isDelete == true)
+            {
+                MessageBox.Show("삭제버튼클릭!");
+            }
+            else
+            {
+                
+            }
+
+        }
     }
 }
